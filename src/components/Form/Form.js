b7ca92761064a53be442f69872676b8fd247aa75
Denys Filichkin <denys.philichkin@gmail.com>
2021-11-05 20:19:38 +0200

    end work

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,56 +1,91 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 
+import shortid from 'shortid';
+import { useDispatch, useSelector } from 'react-redux';
+
+// import PropTypes from 'prop-types';
 import s from './Form.module.css';
+import { addContact } from '../redux/contacts/contacts-actions';
 
-class Form extends Component {
-  state = {
-    name: '',
-    number: '',
+const initialState = {
+  name: '',
+  number: '',
+};
+
+function Form() {
+  const [state, setState] = useState(initialState);
+
+  const contacts = useSelector((state) => state.contacts.items);
+  const dispatch = useDispatch();
+  const onSubmit = (name, number) => dispatch(addContact(name, number));
+
+  const nameId = shortid.generate();
+  const numberId = shortid.generate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prev) => ({ ...prev, [name]: value }));
   };
 
-  handleChange = (e) => {
-    // const { name, value } = e.target;
-    this.setState({ [e.target.name]: e.target.value });
+  const reset = () => {
+    setState((prev) => ({ ...prev, name: '', number: '' }));
   };
 
-  handleSubmit = (e) => {
+  const addNoRepeatContact = (state, contacts) => {
+    const { name, number } = state;
+    if (
+      contacts.some(
+        (contacts) => contacts.name.toLowerCase() === name.toLowerCase(),
+      )
+    ) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    if (contacts.some((contacts) => contacts.number === number)) {
+      alert(`${number} is already in contacts`);
+      return;
+    }
+
+    onSubmit(state);
+    reset();
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+
+    addNoRepeatContact(state, contacts);
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
+  const { name, number } = state;
 
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <form onSubmit={this.handleSubmit} className={s.form}>
-        <label className={s.label}>
-          Name
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={s.form}>
+        <label htmlFor={nameId} className={s.label}>
+          <span>Name</span>
           <input
             type="text"
             name="name"
             value={name}
-            onChange={this.handleChange}
+            onChange={handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
             required
+            id={nameId}
             className={s.input}
           />
         </label>
-        <label className={s.label}>
-          Number
+        <label htmlFor={numberId} className={s.label}>
+          <span>Number</span>
           <input
             type="tel"
             name="number"
             value={number}
-            onChange={this.handleChange}
+            onChange={handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
             required
+            id={numberId}
             className={s.input}
           />
         </label>
@@ -58,8 +93,8 @@ class Form extends Component {
           Add contact
         </button>
       </form>
-    );
-  }
+    </>
+  );
 }
 
 export default Form;
